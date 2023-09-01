54cba445fc951d275d927c23801b1682f6258030
Attila Kerekes <133269554+attila-kerekes@users.noreply.github.com>
2023-09-01 16:01:00 +0200

    half-ready "Patch" task

--- client/public/script.js
+++ client/public/script.js
@@ -4,67 +4,84 @@ console.log("The site is loaded!")
 
 
 
-//Serve and Fetch
-    // Define the API URL
+// Serve /Serve and Fetch
+// Define the API URL
 const url = 'http://127.0.0.1:3000/users'; //The url constant specifies the URL of our API endpoint that returns the users in JSON format.
 
-    // Define a function to generate user HTML
+// Define a function to generate user HTML
 const userHTML = user => `<div class="user"><span class="user-id">${user.id}</span> ${user.name}</div>`; //The userHTML function takes a single user object as an argument and returns an HTML string representing that user as a div element. 
-    // Define a function to generate users HTML
+// Define a function to generate users HTML
 const usersHTML = users => `<div id="users">${users.map(user => userHTML(user)).join("")}</div>`; //The usersHTML (watch the plural) function takes an array of user objects as an argument and returns an HTML string representing all of those users as a div element.
 
-//Send data to server / Form
-    // Define a function to generate input HTML
+//Send data to server / Form, //Send data to server / Patch
+// Define a function to generate input HTML
 const inputHTML = name => `<input placeholder="Write the name here" value="${name}">`;
-    // Define a function to generate button HTML
+// Define a function to generate button HTML
 const buttonHTML = (text, method) => `<button type="submit" data-method="${method}">${text}</button>`;
 
-    // Define a function to generate form HTML
+// Define a function to generate form HTML
 const formHTML = (user) => `
-<form id="form" data-id="${user.id}">
-${inputHTML(user.name)}
-${buttonHTML("Save", "")}
-</form>
-`;
+    <form id="form" data-id="${user.id}">
+      ${inputHTML(user.name)}
+      ${buttonHTML("Save", "PATCH")}
+    </form>
+  `;
 
-//Serve and Fetch
-    // Define a function to fetch user data from the API
-const fetchData = async (url, id) => { //The fetchData function is an asynchronous function that takes a URL as an argument and uses the fetch function to make a request to that URL. If the request is successful, the function parses the response as JSON and returns it. If there is an error, the function logs the error to the console.
-  try {
-    const response = await fetch(id !== undefined ? `${url}/${id}` : url);
-    return await response.json();
+// Serve / Serve and Fetch, //Send data to server / Form, //Send data to server / Patch
+// Define a function to fetch user data from the API
+const fetchData = async (url, id, method = "GET", body = {name: ""}) => { //The fetchData function is an asynchronous function that takes a URL as an argument and uses the fetch function to make a request to that URL. If the request is successful, the function parses the response as JSON and returns it. If there is an error, the function logs the error to the console.
 
-  } catch (error) {
-    console.error(error);
-  }
-};
+    try {
+        const response = await fetch(id !== undefined ? `${url}/${id}` : url, method === "GET" ? {method} : {method, headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(body)} );
+        return await response.json();
+      
+      } catch (error) {
+        console.error(error);
+      }
+    }
 
-    // Define the main function
-const main = async _ => { //The main function is an asynchronous function that uses the fetchData function to load user data from our API endpoint specified by the url constant. When the user data is loaded, the function inserts it into the DOM as HTML using the insertAdjacentHTML method on an element with an ID of root
-  const root = document.getElementById("root");
-  const users = await fetchData(url);
-  root.insertAdjacentHTML("beforeend", usersHTML(users));
-  root.insertAdjacentHTML("beforeend", formHTML({ id: 0, name: "" }));
-  document.addEventListener("click", handleClick);
-};
-
+//Send data to server / Form    
     // Define a function to handle user clicks
 const handleClick = async ({ target }) => {
-  const userTarget = target.classList.contains('user') ? target : target.closest('.user');
+    const userTarget = target.classList.contains('user') ? target : target.closest('.user');
 
-  if (userTarget) {
-    const userId = userTarget.querySelector(".user-id").textContent;
-    const userData = await fetchData(url, userId);
+    if (userTarget) {
+        const userId = userTarget.querySelector(".user-id").textContent;
+        const userData = await fetchData(url, userId);
 
-    const response = await fetch(`${url}/${userId}`);
-    const data = await response.json();
+        const response = await fetch(`${url}/${userId}`);
+        const data = await response.json();
 
-    const inputElement = document.querySelector('input');
-    inputElement.value = data.name;
+        const inputElement = document.querySelector('input');
+        inputElement.value = data.name;
 
-    document.getElementById("form").outerHTML = formHTML(userData);
-    document.getElementById('form').dataset.id = userId;
-  }
+        document.getElementById("form").outerHTML = formHTML(userData);
+        document.getElementById('form').dataset.id = userId;
+    }
+};
+
+//Send data to server / Patch
+const handleInput = ({ target }) => {
+    target.setAttribute("value", target.value);
+}
+
+const handleSubmit = e => {
+    e.preventDefault();
+
+    const method = e.submitter.getAttribute("data-method");
+
+    fetchData(url, e.target.getAttribute("data-id"), method, method === "PATCH" ? {name: e.target.querySelector("input").value} : {name: ""})
+}
+
+// Define the main function
+const main = async _ => { //The main function is an asynchronous function that uses the fetchData function to load user data from our API endpoint specified by the url constant. When the user data is loaded, the function inserts it into the DOM as HTML using the insertAdjacentHTML method on an element with an ID of root
+    const root = document.getElementById("root");
+    const users = await fetchData(url);
+    root.insertAdjacentHTML("beforeend", usersHTML(users));
+    root.insertAdjacentHTML("beforeend", formHTML({ id: 0, name: "" }));
+    document.addEventListener("click", handleClick);
+    window.addEventListener("input", handleInput);
+    window.addEventListener("submit", handleSubmit);
 };
 
 window.addEventListener("load", main); //The main function is called when the "load" event fires on the window object (which means that the entire page is loaded).
--- index.js
+++ index.js
@@ -5,12 +5,19 @@ import fs from "fs";
 
 const app = express();
 
-//Client files
+// Serve / Serve files
+
+  //Client files
 import path from "path";
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+
+//Send data to server / Patch
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/pub', express.static(path.join(__dirname, 'client', 'public'))); //We use the use method with two arguments to serve the static files from the public directory. 
 
@@ -30,7 +37,9 @@ app.get('/users', (req, res) => { //This code creates an Express.js web server w
     });
 });
 
-//Find the user by id
+// Serve / Serve files
+
+  //Find the user by id
 
 app.get('/users/:userId', (req, res) => { //and /users/:userId. the second endpoint serves data for a specific user based on their user ID.
     fs.readFile('./users.json', 'utf8', (err, data) => { //The fs.readFile method is used to read the user data from the users.json file. 
@@ -51,3 +60,29 @@ app.get('/users/:userId', (req, res) => { //and /users/:userId. the second endpo
 app.get('/edit', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'index.html'));
   });
+
+//Send data to server / Patch
+app.patch('/users/:userId', (req, res) => {
+  fs.readFile(dataRoute, 'utf8', (err, data) => {
+    if (err) throw err;
+
+    const {users} = JSON.parse(data);
+    const userId = parseInt(req.params.userId);
+    const user = users.find(user => user.id === userId);
+
+    if (user) {
+      user.name = req.body.name;
+
+      fs.writeFile(dataRoute, JSON.stringify({users}), (err) => {
+        if (err) throw err;
+      });
+      
+      res.send({state: "DONE"});
+    } else {
+      res.status(404).send({state: 'User not found'});
+    }
+  });
+});
+
+
+
